7aaf6a841c7796879cdc452435456c847ae86d40
Afonso Azaruja <92178181+afonsoazaruja@users.noreply.github.com>
2022-05-28 17:49:07 +0100

    Update projeto.asm

--- projeto.asm
+++ projeto.asm
@@ -21,40 +21,108 @@ TEC_COL    EQU 0E000H  ; endereço das colunas do teclado (periférico PIN)
 LINHA      EQU 8       ; linha a testar 8 (linha, 1000b)
 MASCARA    EQU 0FH     ; necessário para isolar os bits de 4-7
 
-TEC_MOV_ESQ    EQU 0       ; tecla de movimento do rover para a esquerda ('0')
-TEC_MOV_DIR    EQU 2       ; tecla de movimento do rover para a direita ('2')
-TEC_DISPARA    EQU 1       ; tecla de disparo do rover ('1')
-TEC_INICIO     EQU 0CH     ; tecla de comecar o jogo ('c')
-TEC_PAUSA      EQU 0DH     ; tecla de suspender/continuar o jogo ('d')
-TEC_FIM        EQU 0EH     ; tecla de terminar o jogo ('e')
+TEC_MOV_ESQ    EQU 0        ; tecla de movimento do rover para a esquerda ('0')
+TEC_MOV_DIR    EQU 2        ; tecla de movimento do rover para a direita ('2')
+TEC_DISPARA    EQU 1        ; tecla de disparo do rover ('1')
+TEC_INICIO     EQU 0CH      ; tecla de comecar o jogo ('c')
+TEC_PAUSA      EQU 0DH      ; tecla de suspender/continuar o jogo ('d')
+TEC_FIM        EQU 0EH      ; tecla de terminar o jogo ('e')
 
-MIN_COLUNA  EQU 0   ; número mínimo da coluna
-MAX_COLUNA  EQU 63  ; número máximo da coluna
+; ******************************** Definição do MediaCenter ********************************
+
+DEFINE_LINHA    		EQU 600AH      ; endereço do comando para definir a linha
+DEFINE_COLUNA   		EQU 600CH      ; endereço do comando para definir a coluna
+DEFINE_PIXEL    		EQU 6012H      ; endereço do comando para escrever um pixel
+APAGA_AVISO     		EQU 6040H      ; endereço do comando para apagar o aviso de nenhum cenário selecionado
+APAGA_ECRÃ	 		    EQU 6002H      ; endereço do comando para apagar todos os pixels já desenhados
+SELECIONA_CENARIO_FUNDO EQU 6042H      ; endereço do comando para selecionar uma imagem de fundo
+
+MIN_COLUNA		EQU 0		    ; número da coluna mais à esquerda que o objeto pode ocupar
+MAX_COLUNA		EQU 64         ; número da coluna mais à direita que o objeto pode ocupar
+
+; *******************************************************************************************
+
+ATRASO			EQU	2500H		    ; atraso para limitar a velocidade de movimento do boneco
+
+LINHA_ROVER        	EQU 28          ; linha do boneco
+COLUNA_ROVER		EQU 30          ; coluna do boneco
+
+ALTURA_ROVER        EQU 4           ; altura do rover
+LARGURA_ROVER	    EQU	5	        ; largura do rover
+COR_PIXEL_ROVER		EQU	0FFF0H      ; cor do pixel do rover: vermelho em ARGB (opaco e vermelho no máximo, verde e azul a 0)
+
+LINHA_MET_BOM       EQU 0           ; linha do meteoro bom
+COLUNA_MET_BOM      EQU 16          ; coluna do meteoro bom
+
+ALTURA_METEORO_BOM  EQU 5           ; altura do meteoro bom
+LARGURA_METEORO_BOM EQU 5           ; largura do meteoro bom
+COR_PIXEL_MET_BOM   EQU 0F0F0H      ; cor do pixel do meteoro bom: verde
+
+; ******************************************************************************
+; * Dados 
+; ******************************************************************************
+PLACE       1000H
+pilha:
+	STACK 100H      ; espaço reservado para a pilha 
+					; (200H bytes, pois são 100H words)
+SP_inicial:			; este é o endereço (1200H) com que o SP deve ser 
+					; inicializado. O 1.º end. de retorno será 
+					; armazenado em 11FEH (1200H-2)
+
+; ******************************** Definição ROVER ******************************* 
+
+DEF_ROVER:					                                                                                ; tabela que define o rover (cor, largura, pixels)
+	WORD	LARGURA_ROVER, ALTURA_ROVER
+    WORD    0 , 0, COR_PIXEL_ROVER, 0 , 0                                                                   ; Definição da 1ª linha da nave 
+	WORD	COR_PIXEL_ROVER, 0, COR_PIXEL_ROVER, 0, COR_PIXEL_ROVER                                         ; Definição da 2ª linha da nave
+    WORD    COR_PIXEL_ROVER, COR_PIXEL_ROVER, COR_PIXEL_ROVER, COR_PIXEL_ROVER, COR_PIXEL_ROVER             ; Definição da 3ª linha da nave
+    WORD    0, COR_PIXEL_ROVER, 0, COR_PIXEL_ROVER, 0                                                       ; Definição da 4ª linha da nave
+
+POS_ROVER: 
+    WORD    LINHA_ROVER, COLUNA_ROVER
+
+; ******************************* Definição METEORO BOM ******************************* 
+
+DEF_METEORO_BOM:                                                                                            ; tabela que define o meteoro bom (cor, largura, pixels)
+    WORD    LARGURA_METEORO_BOM, ALTURA_METEORO_BOM
+    WORD    0, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, 0                                   ; Definição da 1ª linha do meteoro
+    WORD    COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM   ; Definição da 2ª linha do meteoro 
+    WORD    COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM   ; Definição da 3ª linha do meteoro 
+    WORD    COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM   ; Definição da 4ª linha do meteoro 
+    WORD    0, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, COR_PIXEL_MET_BOM, 0                                   ; Definição da 5ª linha do meteoro
+
+POS_METEORO_BOM:
+    WORD    LINHA_MET_BOM, COLUNA_MET_BOM
 
 ; ******************************************************************************
 ; * CODIGO
 ; ******************************************************************************
 
-PLACE 0
+PLACE 0H
 
 inicio:
-    MOV R2, TEC_LIN    ; endereço da linha do teclado
-    MOV R3, TEC_COL    ; endereço da coluna do teclado
-    MOV R4, DISPLAYS   ; endereço do display
-    MOV R5, MASCARA    ; mascara para tornar bits 4-7 a 0
-    MOV R6, 0          ; contador para saber o numero da linha e coluna
-    MOV R7, 4          ; constante para calcular qual tecla foi pressionada
-
-reset_tecla:
-    MOV R1, LINHA
+    MOV SP, SP_inicial                  ; inicializa o Stack Pointer
+    MOV [APAGA_AVISO], R1	            ; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
+    MOV [APAGA_ECRÃ], R1	            ; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
+	MOV	R0, 0		                    ; cenário de fundo número 0
+    MOV [SELECIONA_CENARIO_FUNDO], R0	; seleciona o cenário de fundo
+    MOV R1, LINHA                       ; linha inicial a analisar
+    MOV R6, 0                           ; contador da linha e da coluna
+    MOV R11, ATRASO
+    MOV R7, [POS_ROVER]                 ; linha do rover
+    MOV R8, [POS_ROVER+2]               ; coluna do rover
+    MOV R9, DEF_ROVER                   ; tabela que define o rover
+    CALL desenha_boneco                 ; desenha o rover
+    MOV R7, [POS_METEORO_BOM]           ; linha do meteoro bom
+    MOV R8, [POS_METEORO_BOM+2]         ; coluna do meteoro bom
+    MOV R9, DEF_METEORO_BOM             ; tabela que define o meteoro bom
+    CALL desenha_boneco                 ; desenha o meteoro bom
 
 espera_tecla:
-    MOVB [R2], R1      ; escrever no periférico de saída, (linha)
-    MOVB R0, [R3]      ; lê o periférico de entrada (coluna)
-    AND R0, R5         ; elimina os bits de 4-7
-    CMP R0, 0          ; se houve tecla pressionada na linha certa
+    MOV R0, 0          ; reset valor de tecla
+    CALL teclado       ; calcula coluna pressionada, regista no R2
+    CMP R2, 0          ; se houve tecla pressionada na linha certa
     JZ linha_depois    ; não há teclado primida? verifica próxima linha
-    MOV R6, 0          ; incializa contador a 0
     JMP calcula_linha  ; se houver tecla primida, obter o valor da linha
 
 linha_depois:
@@ -65,62 +133,216 @@ linha_depois:
 
 calcula_linha:
     SHR R1, 1          ; shift 1 bit para direita até chegar a 0
-    INC R6             ; incrementa o contador para calcular a linha
     CMP R1, 0          ; chegou ao fim?
     JZ mul_tecla       ; se sim, volta para completar a conta
+    INC R0             ; incrementa o contador para calcular a linha
     JMP calcula_linha  ; se não, continua
 
+mul_tecla:
+    ADD R0, R0         ; multiplica por 2
+    ADD R0, R0         ; multiplica por 2 
+
 calcula_coluna:
-    SHR R0, 1          ; shift 1 bit para direita até chegar a 0
-    INC R6             ; incrementa o contador para calcular a coluna 
-    CMP R0, 0          ; chegou ao fim?
-    JZ add_tecla       ; se sim, volta para finalizar a conta
+    SHR R2, 1          ; shift 1 bit para direita até chegar a 0
+    CMP R2, 0          ; chegou ao fim?
+    JZ escolhe_comando ; se chegou ao fim
+    INC R0             ; incrementa o contador para calcular a coluna 
     JMP calcula_coluna ; se não, continua 
 
-mul_tecla:
-    DEC R6             ; decrementar o número da linha pois (0, 1, 2, 3)
-    MOV R1, R6         
-    MUL R1, R7         ; multiplica a linha por 4 --> (n-1)*4
-    MOV R6, 0          ; inicializa o contador a 0, para calcular a coluna
-    JMP calcula_coluna ; obter o valor da coluna
-
-add_tecla:
-    DEC R6             ; decrementar o número da coluna pois (0, 1, 2, 3)
-    ADD R1, R6         ; soma a multiplicação anterior com o número da coluna
-
 escolhe_comando:
-    CMP R1, TEC_MOV_ESQ
+    CMP R0, TEC_MOV_ESQ
     JZ mover_esq            ; se for tecla '0'
-    CMP R1, TEC_MOV_DIR
+    CMP R0, TEC_MOV_DIR
     JZ mover_dir            ; se for tecla '2'
-    CMP R1, TEC_DISPARA     
+    CMP R0, TEC_DISPARA     
     JZ dispara              ; se for tecla '1'
     MOV R11, TEC_INICIO     ; criação de máscara
-    XOR R11, R1             ; se a tecla for filtrada é a tecla correta
+    XOR R11, R0             ; se a tecla for filtrada é a tecla correta
     JZ iniciar_jogo         ; se for tecla 'c'
     MOV R11, TEC_PAUSA      ; atualização de máscara
-    XOR R11, R1             ; se a tecla for filtrada é a tecla correta
+    XOR R11, R0             ; se a tecla for filtrada é a tecla correta
     JZ pausar_jogo          ; se for tecla 'd'
     MOV R11, TEC_FIM        ; atualização de máscara
-    XOR R11, R1             ; se a tecla for filtrada é a tecla correta
+    XOR R11, R0             ; se a tecla for filtrada é a tecla correta
     JZ terminar_jogo        ; se for tecla 'e'
     JMP espera_tecla        ; caso não corresponda a nenhuma tecla
 
 mover_esq:
-    MOV R10, 0
-    JMP reset_tecla
+    MOV R7, [POS_ROVER]     ; linha atual do rover
+    MOV R8, [POS_ROVER+2]   ; coluna atual do rover
+    MOV R9, DEF_ROVER       ; tabela que define o rover
+    CMP R8, MIN_COLUNA      ; o rover esta na coluna 0?
+    JZ espera_tecla         ; se estiver, não faz nada
+    CALL apaga_boneco       ; caso contrário, apaga o rover
+    CALL atraso             ; delay de movimento
+    DEC R8                  ; decrementa a coluna
+    MOV [POS_ROVER+2], R8   ; atualiza a coluna em memória
+    ;MOV R7, [POS_ROVER]     ; linha atual do rover
+    MOV R9, DEF_ROVER       ; tabela que define o rover
+    CALL desenha_boneco     ; desenha o rover na nova posição
+    JMP espera_tecla        ; vai esperar por uma nova tecla
+
 mover_dir:
-    MOV R10, 2
-    JMP reset_tecla
+    MOV R7, [POS_ROVER]     ; linha atual do rover
+    MOV R8, [POS_ROVER+2]   ; coluna atual do rover
+    MOV R9, DEF_ROVER       ; tabela que define o rover
+    ADD R8, 5               ; posição do pixel mais à direita do rover
+    MOV R10, MAX_COLUNA	    ; valor maximo da coluna	
+    CMP R8, R10             ; o rover esta na coluna 64?
+    JZ espera_tecla         ; se estiver, não faz nada
+    ADD R8, -5              ; Restora para a posição do pixel mais à esquerda
+    CALL apaga_boneco       ; caso contrário, apaga o rover
+    CALL atraso             ; delay de movimento
+    INC R8                  ; aumenta a coluna
+    MOV [POS_ROVER+2], R8   ; atualiza a coluna em memória
+    ;MOV R7, [POS_ROVER]     ; linha atual do rover
+    MOV R9, DEF_ROVER       ; tabela que define o rover
+    CALL desenha_boneco     ; desenha o rover na nova posição
+    JMP espera_tecla        ; vai esperar por uma nova tecla
+    JMP espera_tecla
 dispara:
     MOV R10, 1
-    JMP reset_tecla
+    JMP espera_tecla
 iniciar_jogo:
     MOV R10, 0CH
-    JMP reset_tecla
+    JMP espera_tecla
 pausar_jogo:
     MOV R10, 0DH
-    JMP reset_tecla
+    JMP espera_tecla
 terminar_jogo:
     MOV R10, 0EH
-    JMP reset_tecla
+    JMP espera_tecla
+
+; ******************************************************************************
+; TECLADO - Faz uma leitura às teclas de uma linha do teclado e retorna o valor 
+;           lido
+; Argumentos:	R1 - linha a testar (em formato 1, 2, 4 ou 8)
+;
+; Retorna: 	R2 - valor lido das colunas do teclado (0, 1, 2, 4, ou 8)	
+; ******************************************************************************
+teclado:
+    PUSH R5
+	MOV  R3, TEC_LIN   ; endereço do periférico das linhas
+	MOV  R4, TEC_COL   ; endereço do periférico das colunas
+	MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
+	MOVB [R3], R1      ; escrever no periférico de saída (linhas)
+	MOVB R2, [R4]      ; ler do periférico de entrada (colunas)
+	AND  R2, R5        ; elimina bits para além dos bits 0-3
+	POP R5             
+    RET
+
+; **********************************************************************
+; DESENHA_BONECO - Desenha um boneco na linha e coluna indicadas
+;			       com a forma e cor definidas na tabela indicada.
+; Argumentos:   R7 - linha do boneco
+;               R8 - coluna do boneco
+;               R9 - tabela que define o boneco
+;
+; **********************************************************************
+desenha_boneco:
+	PUSH	R1
+	PUSH	R2
+	PUSH	R3
+	PUSH	R4
+    PUSH    R5
+    MOV	R1, [R9]			; obtém a largura do boneco
+	MOV R4, R1              ; guarda a largura do boneco
+    ADD	R9, 2			    ; endereço da altura do boneco (2 porque a largura é uma word)
+    MOV R2, [R9]            ; obtém a altura do boneco
+    MOV R5, R2              ; guarda a altura do boneco
+    ADD R9, 2               ; endereço do 1º pixel
+
+desenha_pixels:       		; desenha os pixels do boneco a partir da tabela
+	MOV	 R3, [R9]			; obtém a cor do próximo pixel do boneco
+	CALL escreve_pixel		; escreve cada pixel do boneco
+	ADD	 R9, 2			    ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+    ADD  R8, 1              ; próxima coluna
+    SUB  R1, 1			    ; menos uma coluna para tratar
+    JNZ  desenha_pixels     ; continua até percorrer toda a largura do objeto
+    ADD  R7, 1              ; próxima linha
+    MOV  R1, R4             ; reset à largura a percorrer
+    SUB  R8, R4             ; volta à coluna original
+    SUB  R2, 1              ; menos uma linha para tratar
+    JNZ  desenha_pixels     ; continua até percorrer toda a altura do objeto
+	MOV  R2, R5             ; reset à altura a percorrer
+    SUB  R7, R5             ; volta à linha original
+    POP R5
+    POP	R4
+	POP	R3
+	POP	R2
+	POP	R1
+	RET
+
+; **********************************************************************
+; ESCREVE_PIXEL - Escreve um pixel na linha e coluna indicadas.
+; Argumentos:   R7 - linha
+;               R8 - coluna
+;               R3 - cor do pixel (em formato ARGB de 16 bits)
+;
+; **********************************************************************
+escreve_pixel:
+	MOV  [DEFINE_LINHA], R7		; seleciona a linha
+	MOV  [DEFINE_COLUNA], R8	; seleciona a coluna
+	MOV  [DEFINE_PIXEL], R3		; altera a cor do pixel na linha e coluna já selecionadas
+	RET
+
+; **********************************************************************
+; APAGA_BONECO - Apaga um boneco na linha e coluna indicadas
+;			     com a forma definida na tabela indicada.
+; Argumentos:   R7 - linha
+;               R8 - coluna
+;               R9 - tabela que define o boneco
+;
+; **********************************************************************
+apaga_boneco:
+	PUSH	R1
+	PUSH	R2
+	PUSH	R3
+	PUSH	R4
+    PUSH    R5
+    MOV	R1, [R9]			; obtém a largura do boneco
+	MOV R4, R1              ; guarda a largura do boneco
+    ADD	R9, 2			    ; endereço da altura do boneco (2 porque a largura é uma word)
+    MOV R2, [R9]            ; obtém a altura do boneco
+    MOV R5, R2              ; guarda a altura do boneco
+    ADD R9, 2               ; endereço do 1º pixel
+
+apaga_pixels:       		; desenha os pixels do boneco a partir da tabela
+	MOV	 R3, 0			    ; obtém a cor do próximo pixel do boneco
+	CALL escreve_pixel		; escreve cada pixel do boneco
+    ADD  R8, 1              ; próxima coluna
+    SUB  R1, 1			    ; menos uma coluna para tratar
+    JNZ  apaga_pixels       ; continua até percorrer toda a largura do objeto
+    ADD  R7, 1              ; próxima linha
+    MOV  R1, R4             ; reset à largura a percorrer
+    SUB  R8, R4             ; volta à coluna original
+    SUB  R2, 1              ; menos uma linha para tratar
+    JNZ  apaga_pixels       ; continua até percorrer toda a altura do objeto
+	MOV  R2, R5             ; reset à altura a percorrer
+    SUB  R7, R5             ; volta à linha original
+    POP R5
+    POP	R4
+	POP	R3
+	POP	R2
+	POP	R1
+	RET
+
+; **********************************************************************
+; ATRASO - Executa um ciclo para implementar um atraso.
+; Argumentos:   R11 - valor que define o atraso
+;
+; **********************************************************************
+atraso:
+	PUSH	R11
+ciclo_atraso:
+	SUB	R11, 1
+	JNZ	ciclo_atraso
+	POP	R11
+	RET
+
+; **********************************************************************
+; VERIFICA_LIMITE_ESQUERDA - Verifica se foi atingido o limite
+;       esquerdo do ecrã
+; Argumentos:    R8 - valor da coluna
+;
+; **********************************************************************
